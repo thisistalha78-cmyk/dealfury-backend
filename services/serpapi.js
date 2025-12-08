71006b06ba9a83299e6e8df7913c82ef164a1eef
thisistalha78-cmyk <thisistalha78@gmail.com>
2025-12-08 23:37:32 +0600

    Update serpapi.js

--- services/serpapi.js
+++ services/serpapi.js
@@ -2,22 +2,21 @@ const fetch = require("node-fetch");
 
 module.exports = async function getDeals(query, location) {
     try {
-        let loc = "";
-        if (location) {
-            loc = `&location=${location.lat},${location.lon}`;
-        }
 
-        const url = `https://serpapi.com/search.json?engine=google_shopping&q=${encodeURIComponent(query)}${loc}&api_key=${process.env.SERP_API_KEY}`;
+        const url = `https://serpapi.com/search.json?engine=google_shopping&q=${encodeURIComponent(query)}&api_key=${process.env.SERP_API_KEY}`;
 
         const r = await fetch(url);
         const json = await r.json();
 
-        // Extract all possible product arrays
+        console.log("SERPAPI RAW JSON:", Object.keys(json));  // DEBUG LOG
+
         const results = [
             ...(json.shopping_results || []),
             ...(json.inline_shopping_results || []),
             ...(json.categorized_shopping_results || [])
         ];
+
+        console.log("FINAL MERGED RESULTS:", results.length); // DEBUG LOG
 
         return results;
 
