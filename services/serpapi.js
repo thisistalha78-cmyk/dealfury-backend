cc5ed1126814684d31f551b4406a3d67a084cc2c
thisistalha78-cmyk <thisistalha78@gmail.com>
2025-12-08 23:21:51 +0600

    Update serpapi.js

--- services/serpapi.js
+++ services/serpapi.js
@@ -12,7 +12,15 @@ module.exports = async function getDeals(query, location) {
         const r = await fetch(url);
         const json = await r.json();
 
-        return json.shopping_results || [];
+        // Extract all possible product arrays
+        const results = [
+            ...(json.shopping_results || []),
+            ...(json.inline_shopping_results || []),
+            ...(json.categorized_shopping_results || [])
+        ];
+
+        return results;
+
     } catch (err) {
         console.log("SerpAPI error:", err);
         return [];
