9c933cc754f13e3ff787b859301f015820fac9a6
thisistalha78-cmyk <thisistalha78@gmail.com>
2025-12-10 11:45:20 +0600

    Update serpapi.js

--- services/serpapi.js
+++ services/serpapi.js
@@ -4,21 +4,16 @@ export default async function getDeals(query) {
   try {
     const url = `https://serpapi.com/search.json?engine=google_product&q=${encodeURIComponent(
       query
-    )}&api_key=${process.env.SERP_API_KEY}`;
+    )}&api_key=bb4940f5be015fe5b1fce30d30be4359c82105d7de6e707b17c8105c81dbcc2b`;
 
     const r = await fetch(url);
     const json = await r.json();
 
-    console.log("SERP JSON KEYS:", Object.keys(json)); // Debug
+    console.log("SERPAPI KEYS:", Object.keys(json));
 
-    // The actual product results come under "products"
-    const results = [
-      ...(json.products || []),
-      ...(json.shopping_results || []),
-      ...(json.inline_shopping_results || []),
-    ];
+    const results = json.products || [];
 
-    console.log("TOTAL RESULTS:", results.length);
+    console.log("FOUND PRODUCTS:", results.length);
 
     return results;
   } catch (err) {
