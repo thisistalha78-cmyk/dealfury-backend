3c9a513aeef4611e2cc98de3c2f7b0e0fd370e98
thisistalha78-cmyk <thisistalha78@gmail.com>
2025-12-10 10:53:16 +0600

    Update serpapi.js

--- services/serpapi.js
+++ services/serpapi.js
@@ -1,24 +1,26 @@
-const fetch = require("node-fetch");
+import fetch from "node-fetch";
 
-async function getDeals(query) {
+export default async function getDeals(query) {
     try {
         const url = `https://serpapi.com/search.json?engine=google_shopping&q=${encodeURIComponent(query)}&api_key=${process.env.SERP_API_KEY}`;
 
         const r = await fetch(url);
         const json = await r.json();
 
+        console.log("RAW SERPAPI:", Object.keys(json)); // DEBUG
+
         const results = [
             ...(json.shopping_results || []),
             ...(json.inline_shopping_results || []),
-            ...(json.categorized_shopping_results || [])
+            ...(json.categorized_shopping_results || []),
         ];
+
+        console.log("FINAL DEAL COUNT:", results.length); // DEBUG
 
         return results;
 
     } catch (err) {
-        console.log("SerpAPI error:", err);
+        console.log("SerpAPI ERROR:", err);
         return [];
     }
 }
-
-module.exports = getDeals;
